715d80d95a03641c1ee0c1598863ae2542a2ce45
crystal <yinc534@newschool.edu>
2016-04-07 11:09:51 -0400

    push

--- alphabet/assets/js/app.js
+++ alphabet/assets/js/app.js
@@ -1,26 +1,48 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 
-// 	$('.letter').addClass("hide")
+	$('.letter').addClass("hide")
 
-// })
+})
 
-// window.onload = runClock;
-
-// function runClock() {
-
-// 	var now = checkTime();
-// 	var letter = document.getElementById('letter');
-// 	var letterClass = []
-
-// 	updateClock();
-// 	setInterval(updateClock, 1000);
-
-// 	function updateClock() {
-// 		now = checkTime();
-
-// 	}
-// }
+window.onload = runClock;
 
 
-// Cookies.set('name', 'value');
+function checkTime() {
+	now = new Date();
+	var date = now.getDate();
+	var month = now.getMonth() + 1;
+	var hours = now.getHours();
+	var minutes = now.getMinutes();
+
+	return {
+		hours : hours,
+		minutes : minutes,
+		date : date,
+		month : month,
+	}
+}
+
+
+function runClock() {
+
+	var now = checkTime();
+	var letter = document.getElementById('letter');
+	var letterClass = []
+
+	updateClock();
+	setInterval(updateClock, 3000);
+
+	function updateClock() {
+		now = checkTime();
+
+		if (now.date == 7 && now.month == 4) {
+			console.log($.cookie("username"));
+		}
+
+	}
+}
+
+
 document.cookie="username=John Doe";
+
+

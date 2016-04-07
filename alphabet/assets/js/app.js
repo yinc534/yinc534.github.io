913472ef13b16c5319e59851053f51120f3165bb
crystal <yinc534@newschool.edu>
2016-04-07 10:40:17 -0400

    push

---
+++ alphabet/assets/js/app.js
@@ -0,0 +1,26 @@
+// $(document).ready(function() {
+
+// 	$('.letter').addClass("hide")
+
+// })
+
+// window.onload = runClock;
+
+// function runClock() {
+
+// 	var now = checkTime();
+// 	var letter = document.getElementById('letter');
+// 	var letterClass = []
+
+// 	updateClock();
+// 	setInterval(updateClock, 1000);
+
+// 	function updateClock() {
+// 		now = checkTime();
+
+// 	}
+// }
+
+
+// Cookies.set('name', 'value');
+document.cookie="username=John Doe";
